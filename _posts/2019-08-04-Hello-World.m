838e38db5dd111d351256589981458665e79abfa
chasemthomas <53712034+chasemthomas@users.noreply.github.com>
2019-08-04 16:12:10 -0700

    Create 2019-08-04-Hello-World.m
    Created Hello world blog entry page

---
+++ _posts/2019-08-04-Hello-World.m
@@ -0,0 +1,11 @@
+---
+layout: post
+title: "Chase Thomas's Hello World page"
+date: 2014-04-30
+---
+
+This page represents my entry back into the world of developing after months of accomplishing little. I intended to make the perfect site for myself but could never get started. I needed to learn this or that language first. I started a project on one framework, only to abandon it for another, which would itself be abandoned. I can't do this to myself anymore. Progress is about incremental accomplishments that build up to something greater. I see now that perfectionism has made it impossible for me to accomplish anything at all. But no more of that. I'm creating this simple homepage with these imperfect words as a first step towards something better.
+
+Chase Thomas, August 4, 2019
+
+Powered by [Jekyll](http://jekyllrb.com) 
